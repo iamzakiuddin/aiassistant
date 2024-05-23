f292a4f6151a3f0ece7a4c3ce3a2c1d67eadcdaa
zaki <>
2024-05-23 12:17:28 +0500

    User creation management done

---
+++ app/src/main/java/com/app/aiassistant/AIActivity.kt
@@ -0,0 +1,11 @@
+package com.app.aiassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ai_activity)
+    }
+}
--- app/src/main/java/com/app/aiassistant/MainActivity.kt
+++ app/src/main/java/com/app/aiassistant/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.app.aiassistant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.app.aiassistant.utils.SharedPreferenceHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private var name: EditText? = null
+    private var about: EditText? = null
+    private var interests: EditText? = null
+    private var hobbies: EditText? = null
+
+    private var save: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (SharedPreferenceHelper.getSharedPreferenceBoolean(this,"isUserSaved",false)){
+            startActivity(Intent(this,AIActivity::class.java))
+            finish()
+        }else{
+            setContentView(R.layout.activity_main)
+            initViews()
+        }
+    }
+
+    private fun saveLocallyAndNavigate() {
+        SharedPreferenceHelper.setSharedPreferenceBoolean(this,"isUserSaved",true)
+        SharedPreferenceHelper.setSharedPreferenceString(this,"name",name?.text?.toString() ?: "")
+        SharedPreferenceHelper.setSharedPreferenceString(this,"about",about?.text?.toString() ?: "")
+        SharedPreferenceHelper.setSharedPreferenceString(this,"interests",hobbies?.text?.toString() ?: "")
+        SharedPreferenceHelper.setSharedPreferenceString(this,"hobbies",hobbies?.text?.toString() ?: "")
+
+        startActivity(Intent(this,AIActivity::class.java))
+        finish()
+    }
+
+    private fun initViews() {
+        name = findViewById(R.id.name)
+        about = findViewById(R.id.about)
+        interests = findViewById(R.id.interests)
+        hobbies = findViewById(R.id.hobbies)
+        save = findViewById(R.id.done)
+
+        save?.setOnClickListener {
+            saveLocallyAndNavigate()
+        }
+
     }
 }
